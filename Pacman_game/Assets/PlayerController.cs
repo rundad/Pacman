7f63fcd0bdfee80c3edba87830d2117add2ed937
rundad <48462000+rundad@users.noreply.github.com>
2020-08-04 10:53:32 +0800

    Implements changing rotation of pacman

--- Pacman_game/Assets/PlayerController.cs
+++ Pacman_game/Assets/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         //The temp vector which holds the next point position that towards to the destination
         Vector2 temp = Vector2.MoveTowards(transform.position, dest, speed);
         //Sets the position to the temp position using rigidbody
@@ -31,22 +31,22 @@ public class PlayerController : MonoBehaviour {
         //keyboard events
         if((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))){//press up arrow key or w
             dest = (Vector2)transform.position + Vector2.up;//move up
-
+            transform.up = Vector2.up;
         }
-        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)))
+        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)))//press down arrow key or s
         {//press up arrow key or w
-            dest = (Vector2)transform.position + Vector2.down;//move up
-
+            dest = (Vector2)transform.position + Vector2.down;//move down
+            transform.up = Vector2.down;
         }
-        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)))
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)))//press left arrow key or a
         {//press up arrow key or w
-            dest = (Vector2)transform.position + Vector2.left;//move up
-
+            dest = (Vector2)transform.position + Vector2.left;//move left
+            transform.up = Vector2.left;
         }
-        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))//press right arrow key or d
         {//press up arrow key or w
-            dest = (Vector2)transform.position + Vector2.right;//move up
-
+            dest = (Vector2)transform.position + Vector2.right;//move right
+            transform.up = Vector2.right;
         }
     }
 }
