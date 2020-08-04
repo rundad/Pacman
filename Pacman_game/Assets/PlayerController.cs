7c0a081ca4b0dde1b6683173ae327f1eb0948c89
rundad <48462000+rundad@users.noreply.github.com>
2020-08-04 11:13:33 +0800

    Updated pacman movement logic, only allow to move when the infront object is not a wall

--- Pacman_game/Assets/PlayerController.cs
+++ Pacman_game/Assets/PlayerController.cs
@@ -27,26 +27,39 @@ public class PlayerController : MonoBehaviour {
         Vector2 temp = Vector2.MoveTowards(transform.position, dest, speed);
         //Sets the position to the temp position using rigidbody
         GetComponent<Rigidbody2D>().MovePosition(temp);
+        //When pacman reaches to the destination position, then able to do the next move
+        if((Vector2)transform.position == dest)
+        {
+            //keyboard events
+            if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && dirValid(Vector2.up))
+            {//press up arrow key or w
+                dest = (Vector2)transform.position + Vector2.up;//move up
+                transform.up = Vector2.up;
+            }
+            if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && dirValid(Vector2.down))//press down arrow key or s
+            {//press up arrow key or w
+                dest = (Vector2)transform.position + Vector2.down;//move down
+                transform.up = Vector2.down;
+            }
+            if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && dirValid(Vector2.left))//press left arrow key or a
+            {//press up arrow key or w
+                dest = (Vector2)transform.position + Vector2.left;//move left
+                transform.up = Vector2.left;
+            }
+            if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && dirValid(Vector2.right))//press right arrow key or d
+            {//press up arrow key or w
+                dest = (Vector2)transform.position + Vector2.right;//move right
+                transform.up = Vector2.right;
+            }
+        }
+        
+    }
 
-        //keyboard events
-        if((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))){//press up arrow key or w
-            dest = (Vector2)transform.position + Vector2.up;//move up
-            transform.up = Vector2.up;
-        }
-        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)))//press down arrow key or s
-        {//press up arrow key or w
-            dest = (Vector2)transform.position + Vector2.down;//move down
-            transform.up = Vector2.down;
-        }
-        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)))//press left arrow key or a
-        {//press up arrow key or w
-            dest = (Vector2)transform.position + Vector2.left;//move left
-            transform.up = Vector2.left;
-        }
-        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))//press right arrow key or d
-        {//press up arrow key or w
-            dest = (Vector2)transform.position + Vector2.right;//move right
-            transform.up = Vector2.right;
-        }
+    //Check pacman's next destination is valid or not
+    private bool dirValid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;//object current position
+        RaycastHit2D hitObj = Physics2D.Linecast(pos + dir, pos);//return the object that are in the way that pacman is going
+        return (hitObj.collider == GetComponent<Collider2D>() || hitObj.collider.gameObject.tag == "pill");
     }
 }
