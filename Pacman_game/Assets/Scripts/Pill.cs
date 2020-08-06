02b674f7f1f2f735dbcac3f37bd042be8c6428fe
rundad <48462000+rundad@users.noreply.github.com>
2020-08-06 12:45:51 +0800

    Implemented pacman collects pills functionality: created new script for pills(Pill.cs)

---
+++ Pacman_game/Assets/Scripts/Pill.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pill : MonoBehaviour {
+
+    //The score that the player will earn when collects the pill
+    private int points = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //When collide with pacman(pacman collects the pill), set current object to inactive(disappear)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.name == "pacman")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
