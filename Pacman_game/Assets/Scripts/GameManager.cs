930e1f434f44630e15c108e185a8d3493d9a8534
rundad <48462000+rundad@users.noreply.github.com>
2020-08-07 10:41:50 +0800

    Implemented two game states of the game: PLAY, GAME_WON

--- Pacman_game/Assets/Scripts/GameManager.cs
+++ Pacman_game/Assets/Scripts/GameManager.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    /// <summary>
+    /// Finite State Machine
+    /// The enum for storing the game states of the game
+    /// </summary>
+    public enum FSMState
+    {
+        PLAY, GAME_WON
+    }
+
+    /// <summary>
+    /// The initial game state when the game started
+    /// </summary>
+    private FSMState gameState = FSMState.PLAY;
+
+    /// <summary>
+    /// The array that stores all of the pill objects in the maze
+    /// </summary>
+    public List<GameObject> pills;
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +31,38 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        switch (gameState)
+        {
+            case FSMState.PLAY: UpdatePlayState(); break;
+            case FSMState.GAME_WON: UpdateGameWonState(); break;
+        }
 	}
+
+    /// <summary>
+    /// This function defines the actions for the PLAY state of FSMState
+    /// This function will check if any of the pill is still active in the maze
+    /// If all of the pills are inactive, the game state will change to GAME_WON state
+    /// </summary>
+    private void UpdatePlayState()
+    {
+        bool flag = false;
+        foreach(GameObject pill in pills)
+        {
+            if (pill.activeSelf)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+        {
+            gameState = FSMState.GAME_WON;
+        }
+    }
+
+    private void UpdateGameWonState()
+    {
+        print("win");
+    }
 }
