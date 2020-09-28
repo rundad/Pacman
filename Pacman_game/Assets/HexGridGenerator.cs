3cdba096882eafbe34435ad5df0f56ce8b618b18
rundad <48462000+rundad@users.noreply.github.com>
2020-09-28 19:06:06 +0800

    Created and added hexgona tile prefab

--- Pacman_game/Assets/HexGridGenerator.cs
+++ Pacman_game/Assets/HexGridGenerator.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class HexGridGenerator : MonoBehaviour {
 
+    public GameObject hexTile;
+
+    int mapWidth = 25;
+    int mapHeight = 12;
+
+    float tileXoffset = 1.8f;
+    float tileZoffset = 1.565f;
+
 	// Use this for initialization
 	void Start () {
-		
+        createHexTileMap();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void createHexTileMap()
+    {
+        for(int x = 0; x <= mapWidth; x++)
+        {
+            for(int z = 0; z <= mapHeight; z++)
+            {
+                GameObject TileGO = Instantiate(hexTile);
+                if(z % 2 == 0)
+                {
+                    TileGO.transform.position = new Vector3(x * tileXoffset, 0, z * tileZoffset);
+                }
+                else
+                {
+                    TileGO.transform.position = new Vector3(x * tileXoffset + tileXoffset / 2, 0, z * tileZoffset);
+                }
+            }
+        }
+    }
 }
