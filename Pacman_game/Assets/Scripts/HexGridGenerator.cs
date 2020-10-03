d703f01262c07d5c637871ab4cf40e986c365a38
rundad <48462000+rundad@users.noreply.github.com>
2020-10-03 14:04:31 +0800

    Added a maze size panel with input fields for the user to set the size for the maze

--- Pacman_game/Assets/Scripts/HexGridGenerator.cs
+++ Pacman_game/Assets/Scripts/HexGridGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HexGridGenerator : MonoBehaviour {
 
@@ -33,6 +35,27 @@ public class HexGridGenerator : MonoBehaviour {
     /// The offset/distance of the tiles in the y axis
     /// </summary>
     private float tileYoffset = 1.565f;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public GameObject sizePanel;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public GameObject environmentPanel;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public GameObject WidthInputField;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public GameObject HeightInputField;
 
     // Use this for initialization
     void Start () {
@@ -83,6 +106,48 @@ public class HexGridGenerator : MonoBehaviour {
     {
         //TODO
         //Load the scene of the hexagonal-grid maze
-        createHexTileMap();
+        //createHexTileMap();
+        SceneManager.LoadScene(1);
+        
+    }
+
+    /// <summary>
+    /// The function that responds to the Hexgonal button click event
+    /// Loads the scene of the hexagonal-grid maze environment
+    /// </summary>
+    public void setMazeSize()
+    {
+        //TODO
+        //Load the scene of the hexagonal-grid maze
+        //createHexTileMap();
+        //SceneManager.LoadScene(1);
+        environmentPanel.SetActive(false);
+        sizePanel.SetActive(true);
+    }
+
+    public void setWidth()
+    {
+        InputField widthIF = WidthInputField.GetComponent<InputField>();
+        string value = widthIF.text;
+        print(value);
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Width is missing");
+            return;
+        }
+        mapWidth = int.Parse(value);
+    }
+
+    public void setHeight()
+    {
+        InputField heightIF = HeightInputField.GetComponent<InputField>();
+        string value = heightIF.text;
+        print(value);
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("height is missing");
+            return;
+        }
+        mapHeight = int.Parse(value);
     }
 }
