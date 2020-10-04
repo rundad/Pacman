66c6ed3da4720166ac9193b1a530d333def04b33
rundad <48462000+rundad@users.noreply.github.com>
2020-10-04 11:09:56 +0800

    Implemented unit testing for the player component

---
+++ Pacman_game/Assets/Scripts/Editor/PlayerComponentTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PlayerComponentTest {
+
+    private Player PlayerComponent;
+
+    [SetUp]
+    public void SetUp()
+    {
+        PlayerComponent = Player.getInstance();
+    }
+    
+    [Test]
+    public void getLivesTest()
+    {
+        int lives = PlayerComponent.getLives();
+        Assert.AreEqual(3, lives);
+    }
+
+    [Test]
+    public void setLivesTest()
+    {
+        PlayerComponent.setLives();
+        int lives = PlayerComponent.getLives();
+        Assert.AreEqual(2, lives);
+    }
+
+    [Test]
+    public void getInstanceTest()
+    {
+        Player player = Player.getInstance();
+        Assert.AreSame(player, PlayerComponent);
+    }
+
+    [Test]
+    public void getSpeedTest()
+    {
+        float speed = PlayerComponent.getSpeed();
+        Assert.AreEqual(0.2f, speed);
+    }
+
+}
